52c6691a3a84b6e7064373259fd632b66bf39eff
Slupik <Slupik98@gmail.com>
2018-09-05 04:56:25 +0200

    bugfix: Properly set buzzer for schedule from SoundBox API

--- JW Downloader/src/jw/kingdom/hall/kingdomtimer/downloader/model/soundbox/schedule/model/TalkToTaskConverter.java
+++ JW Downloader/src/jw/kingdom/hall/kingdomtimer/downloader/model/soundbox/schedule/model/TalkToTaskConverter.java
@@ -14,6 +14,7 @@ class TalkToTaskConverter {
         ScheduleTask task = new ScheduleTask();
         task.setName(talk.getName());
         task.setTime(talk.getMinutes()*60);
+        task.setActiveBuzzer(talk.isStudentTalk());
         return task;
     }
 }
