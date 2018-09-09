5b7c6ce113f5e692c8382bdbf037407747959acb
Slupik <Slupik98@gmail.com>
2018-09-09 02:42:41 +0200

    feat: Add background implementation of instant time direct control

--- JavaFx/src/jw/kingdom/hall/kingdomtimer/javafx/control/time/direct/BtnTimeDirectForInstantController.java
+++ JavaFx/src/jw/kingdom/hall/kingdomtimer/javafx/control/time/direct/BtnTimeDirectForInstantController.java
@@ -3,8 +3,13 @@ package jw.kingdom.hall.kingdomtimer.javafx.control.time.direct;
 import javafx.scene.control.Button;
 import jw.kingdom.hall.kingdomtimer.config.model.Config;
 import jw.kingdom.hall.kingdomtimer.entity.task.ObservableTask;
+import jw.kingdom.hall.kingdomtimer.entity.task.Task;
+import jw.kingdom.hall.kingdomtimer.entity.task.TaskBean;
+import jw.kingdom.hall.kingdomtimer.entity.time.countdown.CountdownController;
+import jw.kingdom.hall.kingdomtimer.entity.time.countdown.CountdownListener;
 import jw.kingdom.hall.kingdomtimer.entity.time.schedule.ScheduleController;
 import jw.kingdom.hall.kingdomtimer.entity.time.schedule.ScheduleListener;
+import jw.kingdom.hall.kingdomtimer.javafx.entity.task.TaskFxBean;
 
 import java.util.List;
 
@@ -12,15 +17,25 @@ import java.util.List;
  * This file is part of KingdomHallTimer which is released under "no licence".
  */
 public class BtnTimeDirectForInstantController extends BtnTimeDirectForObj {
-    public BtnTimeDirectForInstantController(ScheduleController schedule, Config config, Button button) {
+    public BtnTimeDirectForInstantController(CountdownController countdown, Config config, Button button) {
         super(config, button);
-        schedule.addListener(new ScheduleListener() {
+        countdown.addListener(new CountdownListener() {
             @Override
-            public void onListChange(List<ObservableTask> list) {
-                super.onListChange(list);
-                //TODO add event onStart onStop and implement this
-//                loadTask(task);
-//                loadTask(null);
+            public void onTaskStart(Task task) {
+                super.onTaskStart(task);
+                if(task instanceof TaskFxBean) {
+                    loadTask(((TaskFxBean) task));
+                } else if(task instanceof ObservableTask){
+                    loadTask(new TaskFxBean(((ObservableTask) task)));
+                } else {
+                    loadTask(new TaskFxBean(task));
+                }
+            }
+
+            @Override
+            protected void onStop() {
+                super.onStop();
+                loadTask(null);
             }
         });
     }
