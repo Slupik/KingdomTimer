945d1aced30456361054e1a744667b18fb7c2112
Slupik <Slupik98@gmail.com>
2018-12-22 19:40:26 +0100

    test: Improve class checking downloader of a schedule

--- JW Downloader/src/jw/kingdom/hall/kingdomtimer/downloader/model/jw/schedule/JwScheduleDownloaderTest.java
+++ JW Downloader/src/jw/kingdom/hall/kingdomtimer/downloader/model/jw/schedule/JwScheduleDownloaderTest.java
@@ -7,23 +7,23 @@
 
 package jw.kingdom.hall.kingdomtimer.downloader.model.jw.schedule;
 
-import jw.kingdom.hall.kingdomtimer.downloader.entity.*;
+import jw.kingdom.hall.kingdomtimer.downloader.entity.DefaultScheduleTranslator;
+import jw.kingdom.hall.kingdomtimer.downloader.entity.ScheduleDownloader;
+import jw.kingdom.hall.kingdomtimer.downloader.entity.ScheduleTask;
+import jw.kingdom.hall.kingdomtimer.downloader.entity.ScheduleTranslator;
 import jw.kingdom.hall.kingdomtimer.downloader.model.jw.schedule.model.JwScheduleDownloader;
 
 public class JwScheduleDownloaderTest {
+
+    private static final boolean CHECK_SPECIFIC_LINK = true;
+
     public static void main(String[] args) {
         testDownloadingTasks();
     }
 
     private static void testDownloadingTasks() {
-        new JwScheduleDownloader().getUrlForToday("ru", url -> {
-//            url = "https://wol.jw.org/pl/wol/dt/r12/lp-p/2018/9/4";
-//            url = "https://wol.jw.org/en/wol/dt/r1/lp-e/2018/9/5";
-//            url = "https://wol.jw.org/en/wol/dt/r1/lp-e";
-//            url = "https://wol.jw.org/ru/wol/dt/r2/lp-u/2018/9/5";
-
-            System.out.println("url = " + url);
-            ScheduleDownloader.InputData data = new ScheduleDownloader.InputData() {
+        if(CHECK_SPECIFIC_LINK) {
+            new JwScheduleDownloader().downloadWeek(new ScheduleDownloader.InputData() {
                 @Override
                 public boolean isCircuitVisit() {
                     return false;
@@ -36,7 +36,7 @@ public class JwScheduleDownloaderTest {
 
                 @Override
                 public String getDestUrl() {
-                    return url;
+                    return "https://wol.jw.org/pl/wol/dt/r12/lp-p/2019/1/8";
                 }
 
                 @Override
@@ -48,12 +48,51 @@ public class JwScheduleDownloaderTest {
                 public int getTimeToEvaluate() {
                     return 60;
                 }
-            };
-            new JwScheduleDownloader().downloadWeek(data, tasks -> {
-                for(ScheduleTask task:tasks) {
-                    System.out.println(task.getName()+" ("+task.getTime()+")");
+            }, tasks -> {
+                for (ScheduleTask task : tasks) {
+                    System.out.println(task.getName() + " (" + task.getTime() + ") buzzer: "+task.isActiveBuzzer());
                 }
             });
-        });
+        } else {
+            new JwScheduleDownloader().getUrlForToday("ru", url -> {
+//            url = "https://wol.jw.org/pl/wol/dt/r12/lp-p/2018/9/4";
+//            url = "https://wol.jw.org/en/wol/dt/r1/lp-e/2018/9/5";
+//            url = "https://wol.jw.org/en/wol/dt/r1/lp-e";
+//            url = "https://wol.jw.org/ru/wol/dt/r2/lp-u/2018/9/5";
+
+                System.out.println("url = " + url);
+                ScheduleDownloader.InputData data = new ScheduleDownloader.InputData() {
+                    @Override
+                    public boolean isCircuitVisit() {
+                        return false;
+                    }
+
+                    @Override
+                    public String getLangCode() {
+                        return "pl";
+                    }
+
+                    @Override
+                    public String getDestUrl() {
+                        return url;
+                    }
+
+                    @Override
+                    public ScheduleTranslator getTranslator() {
+                        return new DefaultScheduleTranslator();
+                    }
+
+                    @Override
+                    public int getTimeToEvaluate() {
+                        return 60;
+                    }
+                };
+                new JwScheduleDownloader().downloadWeek(data, tasks -> {
+                    for (ScheduleTask task : tasks) {
+                        System.out.println(task.getName() + " (" + task.getTime() + ") buzzer: "+task.isActiveBuzzer());
+                    }
+                });
+            });
+        }
     }
 }
